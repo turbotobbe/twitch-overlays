644f0dab7edea002f0d3f52bcc2c188923817ec6
Thobias Bergqvist <thobias.bergqvist@gmail.com>
2024-03-23 10:03:05 +0100

    testing release process

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+// This is a React component
 function App() {
   const [count, setCount] = useState(0)
 
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+// This is the entry point of your app
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
