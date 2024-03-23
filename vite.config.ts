c6a98fd353179409c9f80c0f639d756873783391
Thobias Bergqvist <thobias.bergqvist@gmail.com>
2024-03-23 11:36:03 +0100

    fixed the root path problem with dotenv

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv';
+dotenv.config();
 
 // https://vitejs.dev/config/
-export default defineConfig({
+const config = defineConfig({
   plugins: [react()],
   base: process.env.BASE_URL || '/',
 })
+
+export default config
