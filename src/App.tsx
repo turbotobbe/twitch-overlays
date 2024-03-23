4446778dde513587495a40dac51678af2b628a9d
Thobias Bergqvist <thobias.bergqvist@gmail.com>
2024-03-23 13:16:16 +0100

    change to hashrouting

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
 // Import your components
 import Home from './components/Home';
